ca0c04d75b06ced4441e35f67a771956ffbc8dad
Sai Hari Krishnan <krishnansaihari@gmail.com>
2025-11-07 18:52:21 -0500

    best time buy and sell stock

---
+++ Sliding Window/BuySellStock.java
@@ -0,0 +1,25 @@
+class Solution {
+
+    public int maxProfit(int[] prices) {
+        int i = 0;
+        int maxProfit = 0;
+        
+        for(int j = i+1 ; j < prices.length ; j++) {
+
+            // Profitable ?
+            if(prices[j] > prices[i]) {
+                int currProfit = prices[j] - prices[i];
+                
+                if(currProfit > maxProfit) {
+                    maxProfit = currProfit;
+                }
+            }
+            else {
+                i = j;
+            }
+        }
+
+        return maxProfit;
+    }
+
+}
